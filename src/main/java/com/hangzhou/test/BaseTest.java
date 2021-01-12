1694908f74891a78bb50030a2c98c34bc0cad8af
lch19970210 <1068333515@qq.com>
2021-01-12 11:18:22 +0800

    新增测试类;

---
+++ src/main/java/com/hangzhou/test/BaseTest.java
@@ -0,0 +1,14 @@
+package com.hangzhou.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 配置Spring和junit整合,junit启动时候家在springIOC容器
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件位置
+@ContextConfiguration({"classpath:application.properties"})
+public class BaseTest  {
+}
---
+++ src/main/java/com/hangzhou/test/serviceTest.java
@@ -0,0 +1,19 @@
+package com.hangzhou.test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class serviceTest extends BaseTest{
+
+//    @Autowired
+//    private AreaService areaService;
+
+    @Test
+    public void testGetAreaList(){
+        int a = 10 / 2;
+    }
+}
