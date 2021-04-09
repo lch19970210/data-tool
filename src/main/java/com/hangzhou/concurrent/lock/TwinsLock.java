00b2a37624a4c2aafde9245038ea5fc274fbf9df
linchenghui <1068333515@qq.com>
2021-04-09 16:24:57 +0800

    自定义同步组件

---
+++ src/main/java/com/hangzhou/concurrent/lock/TwinsLock.java
@@ -0,0 +1,121 @@
+package com.hangzhou.concurrent.lock;
+
+import org.springframework.aop.ThrowsAdvice;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 只允许至多有两个线程访问的锁
+ * @Author linchenghui
+ * @Date 2021/4/9
+ */
+public class TwinsLock implements Lock {
+
+    public final Sync sync = new Sync(2);
+
+    private static final class Sync extends AbstractQueuedSynchronizer{
+
+        /**
+         * 构造器
+         * @param count 资源数
+         */
+        Sync(int count){
+            if(count < 0){
+                throw new IllegalArgumentException("count must large than zero.");
+            }
+            setState(count);
+        }
+
+        @Override
+        protected int tryAcquireShared(int reduceCount) {
+            for (;;){
+                int current = getState();
+                int newCount = current - reduceCount;
+                // 原子性保障
+                if(newCount < 0 || compareAndSetState(current,newCount)){
+                    return newCount;
+                }
+            }
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int returnCount) {
+            for (;;){
+                int current = getState();
+                int newCount = current + returnCount;
+                // 原子性保障
+                if(compareAndSetState(current,newCount)){
+                    return true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void lock() {
+        sync.acquireShared(1);
+    }
+
+    @Override
+    public void unlock() {
+        sync.releaseShared(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    /**
+     * 同一时刻只有两个线程能够获取到锁
+     * @param args  参数
+     * @throws InterruptedException exception
+     */
+    public static void main(String[] args) throws InterruptedException {
+        final Lock lock = new TwinsLock();
+        class Worker extends Thread{
+            @Override
+            public void run() {
+                while (true){
+                    lock.lock();
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        System.out.println(Thread.currentThread().getName());
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            Worker worker = new Worker();
+            worker.setDaemon(true);
+            worker.start();
+        }
+        for (int i = 0; i < 10; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println();
+        }
+    }
+}
